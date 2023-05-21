c1acc04072223b27252ba947d5f6ee11a4d9ac4c
linhdandaegal <linhdan7143@gmail.com>
2023-05-21 14:54:08 +0700

    fix Account

--- Forms/fAccount.Designer.cs
+++ Forms/fAccount.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnUpdate = new System.Windows.Forms.Button();
             this.panel5 = new System.Windows.Forms.Panel();
             this.txtReEnterPass = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -45,6 +44,7 @@
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.lbUsername = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
+            this.btnUpdate = new FontAwesome.Sharp.IconButton();
             this.panel5.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -53,41 +53,31 @@
             this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnUpdate
-            // 
-            this.btnUpdate.Location = new System.Drawing.Point(543, 382);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(94, 29);
-            this.btnUpdate.TabIndex = 0;
-            this.btnUpdate.Text = "Update";
-            this.btnUpdate.UseVisualStyleBackColor = true;
-            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
-            // 
             // panel5
             // 
             this.panel5.Controls.Add(this.txtReEnterPass);
             this.panel5.Controls.Add(this.label4);
-            this.panel5.Location = new System.Drawing.Point(85, 289);
+            this.panel5.Location = new System.Drawing.Point(66, 296);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(552, 62);
+            this.panel5.Size = new System.Drawing.Size(592, 62);
             this.panel5.TabIndex = 5;
             // 
             // txtReEnterPass
             // 
             this.txtReEnterPass.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtReEnterPass.Location = new System.Drawing.Point(222, 16);
+            this.txtReEnterPass.Location = new System.Drawing.Point(253, 16);
             this.txtReEnterPass.Name = "txtReEnterPass";
-            this.txtReEnterPass.Size = new System.Drawing.Size(305, 30);
+            this.txtReEnterPass.Size = new System.Drawing.Size(318, 30);
             this.txtReEnterPass.TabIndex = 1;
             this.txtReEnterPass.UseSystemPasswordChar = true;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(20, 19);
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 13);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(196, 24);
+            this.label4.Size = new System.Drawing.Size(236, 32);
             this.label4.TabIndex = 0;
             this.label4.Text = "Re-enter password:";
             // 
@@ -95,27 +85,27 @@
             // 
             this.panel4.Controls.Add(this.txtNewPass);
             this.panel4.Controls.Add(this.label3);
-            this.panel4.Location = new System.Drawing.Point(85, 221);
+            this.panel4.Location = new System.Drawing.Point(66, 228);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(552, 62);
+            this.panel4.Size = new System.Drawing.Size(592, 62);
             this.panel4.TabIndex = 4;
             // 
             // txtNewPass
             // 
             this.txtNewPass.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNewPass.Location = new System.Drawing.Point(222, 16);
+            this.txtNewPass.Location = new System.Drawing.Point(253, 16);
             this.txtNewPass.Name = "txtNewPass";
-            this.txtNewPass.Size = new System.Drawing.Size(305, 30);
+            this.txtNewPass.Size = new System.Drawing.Size(318, 30);
             this.txtNewPass.TabIndex = 0;
             this.txtNewPass.UseSystemPasswordChar = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(20, 19);
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 13);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(155, 24);
+            this.label3.Size = new System.Drawing.Size(182, 32);
             this.label3.TabIndex = 1;
             this.label3.Text = "New password:";
             // 
@@ -123,27 +113,27 @@
             // 
             this.panel3.Controls.Add(this.txtPassWord);
             this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(85, 153);
+            this.panel3.Location = new System.Drawing.Point(66, 160);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(552, 62);
+            this.panel3.Size = new System.Drawing.Size(592, 62);
             this.panel3.TabIndex = 3;
             // 
             // txtPassWord
             // 
             this.txtPassWord.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassWord.Location = new System.Drawing.Point(222, 16);
+            this.txtPassWord.Location = new System.Drawing.Point(253, 16);
             this.txtPassWord.Name = "txtPassWord";
-            this.txtPassWord.Size = new System.Drawing.Size(305, 30);
+            this.txtPassWord.Size = new System.Drawing.Size(318, 30);
             this.txtPassWord.TabIndex = 1;
             this.txtPassWord.UseSystemPasswordChar = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(20, 19);
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 24);
+            this.label2.Size = new System.Drawing.Size(124, 32);
             this.label2.TabIndex = 0;
             this.label2.Text = "Password:";
             // 
@@ -151,26 +141,26 @@
             // 
             this.panel2.Controls.Add(this.txtDisplayName);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(85, 85);
+            this.panel2.Location = new System.Drawing.Point(66, 92);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(552, 62);
+            this.panel2.Size = new System.Drawing.Size(592, 62);
             this.panel2.TabIndex = 2;
             // 
             // txtDisplayName
             // 
             this.txtDisplayName.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDisplayName.Location = new System.Drawing.Point(222, 16);
+            this.txtDisplayName.Location = new System.Drawing.Point(253, 16);
             this.txtDisplayName.Name = "txtDisplayName";
-            this.txtDisplayName.Size = new System.Drawing.Size(305, 30);
+            this.txtDisplayName.Size = new System.Drawing.Size(318, 30);
             this.txtDisplayName.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 24);
+            this.label1.Size = new System.Drawing.Size(168, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Display name:";
             // 
@@ -178,27 +168,27 @@
             // 
             this.panel1.Controls.Add(this.txtUsername);
             this.panel1.Controls.Add(this.lbUsername);
-            this.panel1.Location = new System.Drawing.Point(85, 17);
+            this.panel1.Location = new System.Drawing.Point(66, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(552, 62);
+            this.panel1.Size = new System.Drawing.Size(592, 62);
             this.panel1.TabIndex = 1;
             // 
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Location = new System.Drawing.Point(222, 16);
+            this.txtUsername.Location = new System.Drawing.Point(253, 16);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.ReadOnly = true;
-            this.txtUsername.Size = new System.Drawing.Size(305, 30);
+            this.txtUsername.Size = new System.Drawing.Size(318, 30);
             this.txtUsername.TabIndex = 1;
             // 
             // lbUsername
             // 
             this.lbUsername.AutoSize = true;
-            this.lbUsername.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbUsername.Location = new System.Drawing.Point(20, 19);
+            this.lbUsername.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername.Location = new System.Drawing.Point(20, 13);
             this.lbUsername.Name = "lbUsername";
-            this.lbUsername.Size = new System.Drawing.Size(112, 24);
+            this.lbUsername.Size = new System.Drawing.Size(132, 32);
             this.lbUsername.TabIndex = 1;
             this.lbUsername.Text = "Username:";
             // 
@@ -216,6 +206,22 @@
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(731, 427);
             this.panel6.TabIndex = 0;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(153)))), ((int)(((byte)(76)))));
+            this.btnUpdate.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnUpdate.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.btnUpdate.IconColor = System.Drawing.Color.Black;
+            this.btnUpdate.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnUpdate.Location = new System.Drawing.Point(503, 375);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(134, 40);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // fAccount
             // 
@@ -242,7 +248,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.TextBox txtReEnterPass;
         private System.Windows.Forms.Label label4;
@@ -259,5 +264,6 @@
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label lbUsername;
         private System.Windows.Forms.Panel panel6;
+        private FontAwesome.Sharp.IconButton btnUpdate;
     }
 }
